4b651601c947cf2f6c344a224a807d9d34d171a6
Sumit <sumitdhamsaniya1@gmail.com>
2022-05-12 14:23:20 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react'
+import EmojiData from './Emoji.json';
 
-function App() {
+const App = () => {
+  const [search,setSearch] = useState("");
+  const [data,setData] = useState([]);
+
+  useEffect(()=>{
+      const newData = EmojiData.filter(emoji => emoji.title.toLowerCase().includes(search.toLowerCase()));
+      setData(newData);
+  },[search])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1> Emoji Search</h1>
+        <input size="30" type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+        </center>
+        {data.map(emoji =>
+          <div className="card" key={emoji.title}>
+          <div className="card-body" onClick={() => {navigator.clipboard.writeText(emoji.name);alert("Emoji Copy")}}>
+            {emoji.symbol} {emoji.title}
+          </div>
+        </div>
+        )}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+//   const [datas, setData] = useState([])
+//   const 
+
+//   useEffect(() => {
+//     fetch("https://reqres.in/api/users?page=2")
+
+//       .then(res => res.json())
+//       .then(
+//         (result) => {
+//           setIsLoaded(true);
+//           setItems(result.data);
+//         }
+//       )
+  
+//   }, [])
+//   return (
+//     <div>
+//       {datas.map((dat) => {
+//         <li>{dat.email}</li>
+//       })}
+//     </div>
+//   )
+// }
+
+// export default App
